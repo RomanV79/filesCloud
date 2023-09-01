6f9cb762479e3526b7718c345fa705718591f39a
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-09-01 14:47:00 +0300

    ref: rename several items

--- src/main/java/ru/vlasov/fileclouds/FilesCloudApplication.java
+++ src/main/java/ru/vlasov/fileclouds/FilesCloudApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class FileCloudsApplication {
+public class FilesCloudApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(FileCloudsApplication.class, args);
+        SpringApplication.run(FilesCloudApplication.class, args);
     }
 
 }
--- src/test/java/ru/vlasov/fileclouds/FilesCloudApplicationTests.java
+++ src/test/java/ru/vlasov/fileclouds/FilesCloudApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class FileCloudsApplicationTests {
+class FilesCloudApplicationTests {
 
     @Test
     void contextLoads() {

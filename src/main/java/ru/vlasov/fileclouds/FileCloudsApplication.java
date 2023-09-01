98ab57fa147d7b58a57257b8c573705adaa8f75d
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-09-01 14:04:49 +0300

    feat: init commit

---
+++ src/main/java/ru/vlasov/fileclouds/FileCloudsApplication.java
@@ -0,0 +1,13 @@
+package ru.vlasov.fileclouds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileCloudsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileCloudsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vlasov/fileclouds/FileCloudsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vlasov.fileclouds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileCloudsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

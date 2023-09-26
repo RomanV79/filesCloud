7e790790ee66b8e642a6a0b120adcf698cebbfe5
RomanV79 <121058298+RomanV79@users.noreply.github.com>
2023-09-26 17:48:23 +0300

    feat: inject .env param for minio

--- src/main/java/ru/vlasov/fileclouds/config/MinioConfig.java
+++ src/main/java/ru/vlasov/fileclouds/config/MinioConfig.java
@@ -1,25 +1,29 @@
 package ru.vlasov.fileclouds.config;
 
 import io.minio.MinioClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@Slf4j
 public class MinioConfig {
 
     @Value("${minio.endpoint}")
     private String endpoint;
-    @Value("${minio.user}")
-    private String user;
-    @Value("${minio.password}")
-    private String password;
+    @Value("${minio.access_key}")
+    private String accessKey;
+    @Value("${minio.secret_key}")
+    private String secretKey;
 
     @Bean
     public MinioClient minioClient() {
+//        log.info("minio user -> {}", user);
+//        log.info("minio password -> {}", password);
         return MinioClient.builder()
                 .endpoint(endpoint)
-                .credentials(user, password)
+                .credentials(accessKey, secretKey)
                 .build();
     }
 }
